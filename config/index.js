2004f3aadefe2cd6bdaa725cf1fe82a39544f54b
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2021-09-03 19:12:02 +0700

    experience

--- config/index.js
+++ config/index.js
@@ -47,7 +47,7 @@ module.exports = {
   // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
   // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
   mediumRssFeed:
-    "https://dickidarmawansaputra.github.io/page-data/experience/experience-data.json",
+    "https://dickidarmawansaputra.github.io/experience/experience-data.json",
   // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
 
   shownArticles: 4,
