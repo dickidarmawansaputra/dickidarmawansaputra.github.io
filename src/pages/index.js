8bb8d59398071b1929e947bbab1bc621c6a4bb1e
Dicki Darmawan Saputra <dickidarmawansaputra@student.untan.ac.id>
2023-11-09 17:59:22 +0700

    patch: patch package v8.0.0

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    AboutSection,
+    ContactSection,
+    HeroSection,
+    InterestsSection,
+    Page,
+    ProjectsSection,
+    Seo,
+} from 'gatsby-theme-portfolio-minimal';
+
+export default function IndexPage() {
+    return (
+        <>
+            <Seo title="Dicki Darmawan Saputra - Personal Portfolio" />
+            <Page useSplashScreenAnimation>
+                <HeroSection sectionId="hero" />
+                <AboutSection sectionId="about" heading="About Me" />
+                <InterestsSection sectionId="skills" heading="Skills" subHeading="Tech Stack" subSubHeading="Learned Tech Stack" />
+                <ProjectsSection sectionId="work" heading="Work Experience" />
+                <ContactSection sectionId="contact" heading="Connect With Me ?" />
+            </Page>
+        </>
+    );
+}
